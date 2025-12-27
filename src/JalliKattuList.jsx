2c5c94601717b5ed207a70e9948da50ef4056d1f
Athisankar-ssk <sankarathi49@gmail.com>
2025-12-27 12:18:35 +0530

    Dynamic Creation List

---
+++ src/JalliKattuList.jsx
@@ -0,0 +1,36 @@
+
+import Vellaiyan from './assets/Vellaiyan.jpg'
+import Appu from './assets/Appu.jpg'
+import Kaari from './assets/Kaari.jpg'
+import Khansai from './assets/Khansai.jpeg'
+import Komban from './assets/Komban.jpg'
+import Maruthu from './assets/Maruthu.jpg'
+import Pettaikali from './assets/Pettaikali.jpg'
+import Sevalai from './assets/Sevalai.jpg'
+import Theni from './assets/Theni.jpeg'
+import Jallikattu from './jalli'
+function JallikattuLists(){
+
+    const JallikattLine=[
+           {BullName:"கரூர் வெள்ளை", img:Vellaiyan ,OwnerName:"எஸ்.பி குமார்", District:"கரூர் மாவட்டம்" },
+           {BullName:"செவாலை", img:Sevalai, OwnerName:"ப.ராஜசேகர்", District:"மதுரை மாவட்டம்" },
+           {BullName:"வெள்ளையன்", img:Khansai, OwnerName:"எஸ்.சுரேஷ் குமார்", District:"கான்சாபுரம்"},
+           {BullName:"பேட்டைக்காளி", img:Pettaikali, OwnerName:"செந்தில் தொண்டமான்", District:"இலங்கை"},
+           { BullName:"கொம்பன்", img:Komban, OwnerName:"சி.விஜயபாஸ்கர்", District:"புதுக்கோட்டை மாவட்டம்"},
+           {BullName:"அப்பு", img:Appu, OwnerName:"ப.ராஜசேகர்", District:"மதுரை மாவட்டம்"},
+           {BullName:"அசுரன்", img:Theni, OwnerName:"G கார்த்தி", District:"தேனி மாவட்டம்"},
+           { BullName:"காரி", img:Kaari, OwnerName:"ப.ராஜசேகர்", District:"மதுரை மாவட்டம்"},
+           {BullName:"மருது", img:Maruthu, OwnerName:"டேவிட்", District:"புதுக்கோட்டை மாவட்டம்"}
+    ]
+
+    const BUllList=JallikattLine.map((bull)=>
+        <Jallikattu BullName={bull.BullName} img={bull.img} OwnerName={bull.OwnerName} District={bull.District}/>
+    )
+
+ return(
+        <>
+        {BUllList}
+        </>   
+ );
+}
+export default JallikattuLists;
--- src/jalli.jsx
+++ src/jalli.jsx
@@ -4,7 +4,7 @@ function Jallikattu(pors){
         <div className="full">
             <div className='Bull'>
             <img src={pors.img} alt="" />
-            <button className="bull-btn">{pors.BullName}</button>
+            <button className="bull-btn" >{pors.BullName}</button>
             <h4>{pors.OwnerName}</h4>
             <h4>{pors.District}</h4>
             </div>
